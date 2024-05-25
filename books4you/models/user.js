db4afd731ac115133c1eb5c09b4e93ab6b967da8
jasimSalman <jassimmo7amed+1@gmail.com>
2024-05-25 14:12:06 +0300

    create user schema

---
+++ books4you/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    name: String,
+    googleId: {
+      type: String,
+      required: true
+    },
+    email: String,
+    avatar: String,
+    type: { type: Boolean, default: false }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
