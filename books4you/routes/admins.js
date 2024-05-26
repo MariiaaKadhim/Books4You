79cc76744ef3bb7c07e8672bd0f65b600817d0ec
MariiaaKadhim <mariiaa.sk@hotmail.com>
2024-05-26 11:04:52 +0300

    added the category page

--- books4you/routes/admins.js
+++ books4you/routes/admins.js
@@ -1,13 +1,11 @@
-var express = require('express')
+var express = require("express")
 var router = express.Router()
-const adminCtrl = require('../controllers/admins')
+const adminCtrl = require("../controllers/admins")
 
-router.get('/index', adminCtrl.index)
+router.get("/", adminCtrl.index)
 
-router.get('/new', adminCtrl.new)
+router.get("/new", adminCtrl.new)
 
-router.post('/', adminCtrl.create)
+router.post("/", adminCtrl.create)
 
-// router.get('/index', adminCtrl.index)
-// router.get('/new', adminCtrl.new)
 module.exports = router
