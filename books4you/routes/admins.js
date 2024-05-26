eaf512aa7632b3a4c423d56bb31cea5118c2b727
jawadbri <jawadalberri@gmail.com>
2024-05-26 09:24:21 +0300

    jasim broke it all

--- books4you/routes/admins.js
+++ books4you/routes/admins.js
@@ -3,14 +3,12 @@ var router = express.Router()
 const adminCtrl = require('../controllers/admins')
 const ensureLoggedIn = require('../config/ensureLoggedIn')
 
-router.get('/index', adminCtrl.index)
+router.get('/', adminCtrl.index)
 
 router.get('/new', ensureLoggedIn, adminCtrl.new)
 
-// router.get('/:id', adminCtrl.show)
-
 router.post('/', ensureLoggedIn, adminCtrl.create)
 
-// router.get('/index', adminCtrl.index)
+// router.get('/:id', adminCtrl.show)
 // router.get('/new', adminCtrl.new)
 module.exports = router
