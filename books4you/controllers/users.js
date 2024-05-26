4e8c947bf37093814227da1178248431cc545401
jawadbri <jawadalberri@gmail.com>
2024-05-26 16:07:15 +0300

    cata works

--- books4you/controllers/users.js
+++ books4you/controllers/users.js
@@ -2,7 +2,7 @@ const Book = require('../models/book')
 
 async function index(req, res) {
   try {
-    let categories = await Book.find({}, { category: 1 })
+    let categories = await Book.find({}, { category: 0 })
     // mongoDB method to call catagories of books
     res.render('users/index', { categories, title: 'Users Page' })
   } catch (e) {
--- books4you/models/book.js
+++ books4you/models/book.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const categorySchema = new Schema({
-  category: {
-    type: String,
-    required: true
-  },
-  id: {
-    type: Number,
-    required: true
-  }
-})
+// const categorySchema = new Schema({
+//   category: {
+//     type: String,
+//     required: true
+//   },
+//   id: {
+//     type: Number,
+//     required: true
+//   }
+// })
 
 const bookSchema = new Schema(
   {
     name: String,
-    category: [categorySchema],
+    category: String,
     author: String,
     publishDate: Date,
     summary: String,
